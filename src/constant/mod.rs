fba2ac53e075603aa89c34b46aa6b7565eb59f4b
尤雨东 <root@ltpp.vip>
2024-12-31 21:15:13 +0800

    feat:file type

--- src/constant/mod.rs
+++ src/constant/mod.rs
@@ -1,6 +1,7 @@
 pub mod common;
+pub mod content_type_value;
+pub mod file_extension;
 pub mod header_key;
 pub mod header_value;
 pub mod method;
-pub mod mime;
 pub mod protocol;
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
 pub(crate) mod constant;
 
-pub use constant::{common::*, header_key::*, header_value::*, method::*, mime::*, protocol::*};
+pub use constant::{
+    common::*, content_type_value::*, file_extension::*, header_key::*, header_value::*, method::*,
+    protocol::*,
+};
