962f7e95e4bd2ffe07a66b5f628b49944dd19622
尤雨东 <root@ltpp.vip>
2025-03-23 12:25:44 +0800

    feat: v1.39.0

--- src/constant/protocol.rs
+++ src/constant/protocol.rs
@@ -1,8 +1,14 @@
 /// A constant representing the "HTTP" protocol.
-pub static HTTP: &str = "HTTP";
+pub static HTTP_UPPERCASE: &str = "HTTP";
 
 /// A constant representing the "HTTPS" protocol.
-pub static HTTPS: &str = "HTTPS";
+pub static HTTPS_UPPERCASE: &str = "HTTPS";
+
+/// A constant representing the "http" protocol.
+pub static HTTP_LOWERCASE: &str = "http";
+
+/// A constant representing the "https" protocol.
+pub static HTTPS_LOWERCASE: &str = "https";
 
 /// Unknown HTTP version
 pub static UNKNOWN_HTTP_VERSION: &str = "";
