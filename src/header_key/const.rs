0ed0948284df7f62c154ed51b74423a083759f3d
尤雨东 <root@ltpp.vip>
2025-05-29 08:21:51 +0800

    feat: v1.43.0

--- src/header_key/const.rs
+++ src/header_key/const.rs
@@ -55,6 +55,15 @@ pub const SEC_WEB_SOCKET_ACCEPT: &str = "sec-websocket-accept";
 /// The HTTP header field name `sec-websocket-key`.
 pub const SEC_WEBSOCKET_KEY: &str = "sec-websocket-key";
 
+/// The HTTP header field name `sec-websocket-protocol`.
+pub const SEC_WEB_SOCKET_VERSION: &str = "sec-websocket-version";
+
+/// The HTTP header field name `sec-websocket-protocol`.
+pub const SEC_WEB_SOCKET_PROTOCOL: &str = "sec-websocket-protocol";
+
+/// The HTTP header field name `sec-websocket-extensions`.
+pub const SEC_WEB_SOCKET_EXTENSIONS: &str = "sec-websocket-extensions";
+
 /// The HTTP header field name `vary`, used to specify that the response may vary based on certain request headers.
 pub const VARY: &str = "vary";
 
