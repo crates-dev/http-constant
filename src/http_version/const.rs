838330f74870ca1ca5a12c430adc787ad526dfaf
尤雨东 <root@ltpp.vip>
2025-11-25 20:49:38 +0800

    feat: v1.71.0

--- src/http_version/const.rs
+++ src/http_version/const.rs
@@ -6,5 +6,9 @@ pub const HTTP_VERSION_1_0: &str = "HTTP/1.0";
 pub const HTTP_VERSION_1_1: &str = "HTTP/1.1";
 /// HTTP version 2.
 pub const HTTP_VERSION_2: &str = "HTTP/2";
+/// HTTP version 2.0.
+pub const HTTP_VERSION_2_0: &str = "HTTP/2.0";
 /// HTTP version 3.
 pub const HTTP_VERSION_3: &str = "HTTP/3";
+/// HTTP version 3.0.
+pub const HTTP_VERSION_3_0: &str = "HTTP/3.0";
